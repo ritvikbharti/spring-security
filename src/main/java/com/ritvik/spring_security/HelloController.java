a06df20e1b8ccfa9e94f0c85fd221fc3d1bbb72d
ritvikbharti <bhartiritvik1000@gmail.com>
2026-04-22 13:03:07 +0530

    spring-se

---
+++ src/main/java/com/ritvik/spring_security/HelloController.java
@@ -0,0 +1,13 @@
+package com.ritvik.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello Baby";
+    }
+}
